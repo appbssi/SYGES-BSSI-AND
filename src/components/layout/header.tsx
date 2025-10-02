aa9a9861bb4a67ae535be00957043a3b5f473384
Ireel <reelinfo13@gmail.com>
2025-10-02 16:19:06 +0000

    effacé  (_for element h1_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,8 +4,7 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { usePathname, useRouter } from "next/navigation";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { LogOut, User, Swords } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { User } from "lucide-react";
 import { useAuth } from "@/context/auth-context";
 
 const getTitle = (pathname: string) => {
@@ -21,7 +20,6 @@ export function AppHeader() {
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm sm:px-6">
       <SidebarTrigger className="md:hidden" />
-      <h1 className="text-xl font-semibold">{getTitle(pathname)}</h1>
       <div className="ml-auto flex items-center gap-4">
         <Avatar className="h-9 w-9 cursor-pointer">
             <AvatarFallback>
